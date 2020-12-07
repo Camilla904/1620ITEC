06d1ec791912981cb8cdd48db5bf7c0572350bbf
Camilla <64920167+Camilla904@users.noreply.github.com>
2020-12-07 10:33:55 -0500

    Update BasicArray.java

--- ch8/UsingArrays/BasicArray.java
+++ ch8/UsingArrays/BasicArray.java
@@ -6,11 +6,16 @@ public class BasicArray {
  public static void main(String[] args)
  {
   final int LIMIT = 15, MULTIPLE = 10;
-      int[] list = new int[LIMIT];
-// Initialize all of the array values
-for (int index = 0; index < LIMIT; index++)
-list[index] = index * MULTIPLE; //assign to the current index
-list[5] = 999; // change one array value ; the 6th element
-// Print the new array values
-for (int value : list) System.out.print(value + " ");
-} }
+  
+  int[] list = new int[LIMIT];
+
+  // Initialize all of the array values
+  for (int index = 0; index < LIMIT; index++)
+  list[index] = index * MULTIPLE; //assign to the current index
+  list[5] = 999; // change one array value ; the 6th element
+  
+  // Print the new array values
+   for (int value : list) 
+    System.out.print(value + " ");
+  } 
+}
