c5fe1e9dec2501068460326211216789ebadf592
Camilla <64920167+Camilla904@users.noreply.github.com>
2020-12-07 22:43:47 -0500

    Update TwoDArray.java

--- ch8/2dArray/TwoDArray.java
+++ ch8/2dArray/TwoDArray.java
@@ -8,24 +8,22 @@ public class TwoDArray
 
 public static void main(String[] args)
 {
-int[][] table = new int[5][10]; // 5 rows by 10 columns
-
-      // Load the table with values
-for (int row=0; row < table.length; row++)
-// table.length = # of rows
-for (int col=0; col < table[row].length; col++)
-// table[row].length = the row length, i.e. # of columns
-table[row][col] = row * 10 + col; //1st Iter: 0*10+0, 0*10+1, ... 0+10+9
-      // Print the table
+      int[][] table = new int[5][10]; // 5 rows by 10 columns
       
-for (int row=0; row < table.length; row++) 
-{
-for (int col=0; col < table[row].length; col++) 
-
-System.out.print(table[row][col] + "\t");
-System.out.println(); 
-}
-
+      // Load the table with values
+      for (int row=0; row < table.length; row++) // table.length = # of rows
+            
+            for (int col=0; col < table[row].length; col++)// table[row].length = the row length, i.e. # of columns
+                  
+                  table[row][col] = row * 10 + col; //1st Iter: 0*10+0, 0*10+1, ... 0+10+9
+      
+      // Print the table
+      for (int row=0; row < table.length; row++) 
+      {
+            for (int col=0; col < table[row].length; col++) 
+                  System.out.print(table[row][col] + "\t");
+            System.out.println(); 
+      }
 }
 
 }
