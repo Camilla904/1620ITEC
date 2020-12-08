7d8673fbd0b3fc3bcee1814e84f585d18258b97e
Camilla <64920167+Camilla904@users.noreply.github.com>
2020-12-07 22:58:11 -0500

    Update SodaSurvey.java

--- ch8/2dArray/SodaSurvey.java
+++ ch8/2dArray/SodaSurvey.java
@@ -32,7 +32,6 @@ public class SodaSurvey
     DecimalFormat fmt = new DecimalFormat("0.#"); 
     System.out.println("Averages:\n");
     
-    
     for (int soda=0; soda < SODAS; soda++) 
       System.out.println("Soda #" + (soda+1) + ": " +
                          fmt.format((float)sodaSum[soda]/PEOPLE)); //avg soda scores
@@ -41,7 +40,7 @@ public class SodaSurvey
     for (int person=0; person < PEOPLE; person++)
       System.out.println("Person #" + (person+1) + ": " +//each person's
                    fmt.format((float)personSum[person]/SODAS)); // score avg
-}
+  }
 
 }
 
