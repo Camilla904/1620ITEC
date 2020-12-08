7a170d1ee5d3aedd0a60e866e3b9ee4b3f89d7ee
Camilla <64920167+Camilla904@users.noreply.github.com>
2020-12-08 09:39:31 -0500

    Update Message.java

--- ch9Inheritance/Message.java
+++ ch9Inheritance/Message.java
@@ -4,44 +4,41 @@ public class Messages
 //----------------------------------------------------------------- 
 // Creates two objects and invokes the message method in each. 
 //----------------------------------------------------------------- 
-
-public static void main(String[] args)
+   
+   public static void main(String[] args)
    {
       Thought parked = new Thought();
       Advice dates = new Advice();
+      
       parked.message();
       dates.message(); // overridden
-      }
+   }
 }
 
-
+-----------------------------------------------------------------
 public class Thought
-{
-//----------------------------------------------------------------- 
-// Prints a message. 
-//----------------------------------------------------------------- 
-
-public void message()
-{
-System.out.println("I feel like I'm diagonally parked in a " +
-"parallel universe.");
-System.out.println(); }
-}
-
+   {
+      //----------------------------------------------------------------- 
+      // Prints a message. 
+      //----------------------------------------------------------------- 
+      public void message()
+      {
+         System.out.println("I feel like I'm diagonally parked in a " + "parallel universe.");
+         System.out.println(); 
+      }
+       
 public class Advice extends Thought
+
 {
 //----------------------------------------------------------------- 
 // Prints a message. This method overrides the parent's version. 
 //----------------------------------------------------------------- 
-public void message() // method override
-{
-System.out.println("Warning: Dates in calendar are closer " +
-"than they appear."); 
-System.out.println();
-super.message(); // explicitly invokes the parent's version 
-
-
-}
+   public void message() // method override
+   {
+      System.out.println("Warning: Dates in calendar are closer " + "than they appear."); 
+      System.out.println();
+      super.message(); // explicitly invokes the parent's version 
+   }
 }
 
 
