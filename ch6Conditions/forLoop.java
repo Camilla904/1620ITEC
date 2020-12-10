c31543283246990080c08fde2e21f92e80b42a80
Camilla <64920167+Camilla904@users.noreply.github.com>
2020-12-10 09:55:30 -0500

    Update forLoop.java

--- ch6Conditions/forLoop.java
+++ ch6Conditions/forLoop.java
@@ -6,26 +6,29 @@ public class Multiples
 //----------------------------------------------------------------- 
 // Prints multiples of a user-specified number up to a user specified limit. 
 //----------------------------------------------------------------- 
-
-public static void main(String[] args)
-{
-final int PER_LINE = 5;
-int value, limit, mult, count = 0;
-Scanner scan = new Scanner(System.in); 
-System.out.print("Enter a positive value: ");
-value = scan.nextInt(); //e.g. 7
-
-System.out.print("Enter an upper limit: "); 
-limit = scan.nextInt(); //e.g. 400
-System.out.println();
-System.out.println("The multiples of " + value + " between " +
-value + " and " + limit + " (inclusive) are:");
-for (mult = value; mult <= limit; mult += value) {
-System.out.print(mult + "\t");
-// Print a specific number of values per line of output
-count++;
-if (count % PER_LINE == 0) // e.g 1st iteration: 1%5= 0.2 !=0 // set new mult to 7(mult)+7(value)
-
-System.out.println();
-} }
+  
+  public static void main(String[] args)
+  {
+    final int PER_LINE = 5;
+    int value, limit, mult, count = 0;
+    
+    Scanner scan = new Scanner(System.in); 
+    
+    System.out.print("Enter a positive value: ");
+    value = scan.nextInt(); //e.g. 7
+    
+    System.out.print("Enter an upper limit: "); 
+    limit = scan.nextInt(); //e.g. 400
+    
+    System.out.println();
+    System.out.println("The multiples of " + value + " between " +
+                       value + " and " + limit + " (inclusive) are:");
+    for (mult = value; mult <= limit; mult += value) 
+    {
+      System.out.print(mult + "\t");  // Print a specific number of values per line of output
+      count++;
+      if (count % PER_LINE == 0) // e.g 1st iteration: 1%5= 0.2 !=0 // set new mult to 7(mult)+7(value)
+        System.out.println();
+    } 
+  }
 }
